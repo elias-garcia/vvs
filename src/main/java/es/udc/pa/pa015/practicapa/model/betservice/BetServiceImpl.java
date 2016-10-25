5548e9251ca1452e5449b677f09be0c4dee22b80
Elias <elias.garcia@udc.es>
2016-10-25 02:08:41 +0200

    Removed some warnings

--- src/main/java/es/udc/pa/pa015/practicapa/model/betservice/BetServiceImpl.java
+++ src/main/java/es/udc/pa/pa015/practicapa/model/betservice/BetServiceImpl.java
@@ -9,7 +9,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import es.udc.pa.pa015.practicapa.model.betinfo.BetInfo;
 import es.udc.pa.pa015.practicapa.model.betinfo.BetInfoDao;
-import es.udc.pa.pa015.practicapa.model.bettype.BetTypeDao;
 import es.udc.pa.pa015.practicapa.model.typeoption.TypeOption;
 import es.udc.pa.pa015.practicapa.model.typeoption.TypeOptionDao;
 import es.udc.pa.pa015.practicapa.model.userprofile.UserProfile;
@@ -22,46 +21,41 @@ public class BetServiceImpl implements BetService {
 
 	@Autowired
 	private BetInfoDao betInfoDao;
-	
-	@Autowired
-	private BetTypeDao betTypeDao;
-	
+
 	@Autowired
 	private TypeOptionDao typeOptionDao;
-	
+
 	@Autowired
 	private UserProfileDao userProfileDao;
-	
-	public BetInfo createBet(Long userId, Long typeOptionId, double amount)
-			throws InstanceNotFoundException {
-		
+
+	public BetInfo createBet(Long userId, Long typeOptionId, double amount) throws InstanceNotFoundException {
+
 		UserProfile currentUser = userProfileDao.find(userId);
 		TypeOption currentOption = typeOptionDao.find(typeOptionId);
-		
-		BetInfo newBet = new BetInfo(Calendar.getInstance(),amount, currentUser, currentOption);
-		
+
+		BetInfo newBet = new BetInfo(Calendar.getInstance(), amount, currentUser, currentOption);
+
 		betInfoDao.save(newBet);
-		
+
 		return newBet;
 	}
 
-	@Transactional(readOnly=true)
-	public BetInfoBlock findBetsByUserId(Long userId, int startindex, int count)
-			throws InstanceNotFoundException {
+	@Transactional(readOnly = true)
+	public BetInfoBlock findBetsByUserId(Long userId, int startindex, int count) throws InstanceNotFoundException {
 
 		userProfileDao.find(userId);
 
 		/*
-		 * Find count+1 bets to determine if there exist more bets above
-		 * the specified range.
+		 * Find count+1 bets to determine if there exist more bets above the
+		 * specified range.
 		 */
 		List<BetInfo> bets = betInfoDao.findBetsByUserId(userId, startindex, count + 1);
 
 		boolean existMoreBets = bets.size() == (count + 1);
 
 		/*
-		 * Remove the last bet from the returned list if there exist more
-		 * bets above the specified range.
+		 * Remove the last bet from the returned list if there exist more bets
+		 * above the specified range.
 		 */
 		if (existMoreBets) {
 			bets.remove(bets.size() - 1);
--- src/main/java/es/udc/pa/pa015/practicapa/web/pages/admin/AddBetType.java
+++ src/main/java/es/udc/pa/pa015/practicapa/web/pages/admin/AddBetType.java
@@ -10,7 +10,6 @@ import org.apache.tapestry5.annotations.InjectComponent;
 import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.corelib.components.Form;
-import org.apache.tapestry5.corelib.components.TextArea;
 import org.apache.tapestry5.corelib.components.Zone;
 import org.apache.tapestry5.ioc.Messages;
 import org.apache.tapestry5.ioc.annotations.Inject;
@@ -35,153 +34,148 @@ public class AddBetType {
 	private TypeOption option;
 	@Property
 	private boolean showZone = false;
-	
+
 	@Property
 	private String betTypeQuestion;
-	
+
 	@Property
 	private String multipleType;
-	
+
 	@Property
 	private Double odd;
 	@Property
 	private String yesNoSelectModel;
-	
+
 	@Property
 	private String typeOptionNames;
-	
+
 	@Property
 	private String typeOptionOdds;
 	/*
-	@Component(id="typeOptionNames")
-	private TextArea typeOptionNamesTextArea; 
-	
-	@Component(id="typeOptionOdds")
-	private TextArea typeOptionOddsTextArea; 
-	*/
+	 * @Component(id="typeOptionNames") private TextArea
+	 * typeOptionNamesTextArea;
+	 * 
+	 * @Component(id="typeOptionOdds") private TextArea typeOptionOddsTextArea;
+	 */
 	@Component
 	private Form addBetTypeForm;
-	
+
 	@InjectComponent
 	private Zone zone;
-	
+
 	@Inject
 	private Locale locale;
 
 	@Inject
 	private Messages messages;
-	
-	@Inject
-    private Request request;
-	
-	@InjectComponent
-    private Zone betTypeAddedZone;
-	
-	@InjectComponent
-    private Zone formZone;
 
-    @Inject
-    private AjaxResponseRenderer ajaxResponseRenderer;
-    
+	@Inject
+	private Request request;
+
+	@InjectComponent
+	private Zone betTypeAddedZone;
+
+	@InjectComponent
+	private Zone formZone;
+
+	@Inject
+	private AjaxResponseRenderer ajaxResponseRenderer;
+
 	@Inject
 	private EventService eventService;
-	
+
 	void onPrepareForRender() {
-		this.yesNoSelectModel = ("false" + "=" + messages.get("select-no") +
-				"," + "true" + "=" + messages.get("select-yes"));
+		this.yesNoSelectModel = ("false" + "=" + messages.get("select-no") + "," + "true" + "="
+				+ messages.get("select-yes"));
 	}
-	
+
 	void onValidateFromAddBetTypeForm() {
 		if (!addBetTypeForm.isValid()) {
 			return;
 		}
-		
+
 		try {
 			BetType newBetType = new BetType(betTypeQuestion, Boolean.parseBoolean(multipleType),
 					eventService.findEvent(eventId));
 			List<TypeOption> newOptions = new ArrayList<TypeOption>();
-			for(TypeOption opt : options){
-				if(opt != null){
-					opt.setOdd(opt.getOdd()/100);
+			for (TypeOption opt : options) {
+				if (opt != null) {
+					opt.setOdd(opt.getOdd() / 100);
 					opt.setOdd(Double.valueOf(opt.getOdd()));
 					newOptions.add(opt);
 				}
 			}
-			
+
 			eventService.addBetType(eventId, newBetType, newOptions);
-			
+
 		} catch (InstanceNotFoundException ex) {
 		}
 	}
-	
+
 	void onSuccess() {
 		if (request.isXHR()) {
 			showZone = true;
-            ajaxResponseRenderer.addRender(formZone).addRender(betTypeAddedZone);
-        }
+			ajaxResponseRenderer.addRender(formZone).addRender(betTypeAddedZone);
+		}
 	}
-	
+
 	void onFailure() {
-        if (request.isXHR()) {
-            ajaxResponseRenderer.addRender(formZone);
-        }
-    }
+		if (request.isXHR()) {
+			ajaxResponseRenderer.addRender(formZone);
+		}
+	}
+
 	Object onRefresh() {
 		options = new ArrayList<TypeOption>();
-        return request.isXHR() ? formZone.getBody() : null;
-    }
-	public String getUniqueZoneId()
-	   {
-	      return "zone_" + options.indexOf(option);
-	   }
-	public int getId()
-	   {
-	      return options.indexOf(option);
-	   }
-	void onActivate(Long eventId) {	
-		this.eventId = eventId;
-		if(options == null)
-	      {
-	         options = new ArrayList<TypeOption>();
-	      }
+		return request.isXHR() ? formZone.getBody() : null;
 	}
-	
+
+	public String getUniqueZoneId() {
+		return "zone_" + options.indexOf(option);
+	}
+
+	public int getId() {
+		return options.indexOf(option);
+	}
+
+	void onActivate(Long eventId) {
+		this.eventId = eventId;
+		if (options == null) {
+			options = new ArrayList<TypeOption>();
+		}
+	}
+
 	Long onPassivate() {
 		options = new ArrayList<TypeOption>();
 		return eventId;
 	}
-	Object onAddRow()
-   {
-      TypeOption newOpt = new TypeOption();
-      options.add(newOpt);
-      return newOpt;
-   }
-    
-   void onRemoveRow(TypeOption newOpt)
-   {
-      options.set(options.indexOf(newOpt), null);
-   }
-   Object onZoneUpdate(int index)
-   {
-      option = options.get(index);
-      return zone.getBody();
-   }
-   
-   public ValueEncoder<TypeOption> getEncoder()
-   {
-      return new ValueEncoder<TypeOption>()
-      {
- 
-         public String toClient(TypeOption option)
-         {
-            return String.valueOf(options.indexOf(option));
-         }
- 
-         public TypeOption toValue(String clientValue)
-         {
-            return options.get(Integer.parseInt(clientValue));
-         }
- 
-      };
-   }
+
+	Object onAddRow() {
+		TypeOption newOpt = new TypeOption();
+		options.add(newOpt);
+		return newOpt;
+	}
+
+	void onRemoveRow(TypeOption newOpt) {
+		options.set(options.indexOf(newOpt), null);
+	}
+
+	Object onZoneUpdate(int index) {
+		option = options.get(index);
+		return zone.getBody();
+	}
+
+	public ValueEncoder<TypeOption> getEncoder() {
+		return new ValueEncoder<TypeOption>() {
+
+			public String toClient(TypeOption option) {
+				return String.valueOf(options.indexOf(option));
+			}
+
+			public TypeOption toValue(String clientValue) {
+				return options.get(Integer.parseInt(clientValue));
+			}
+
+		};
+	}
 }
